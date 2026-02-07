5d6ae997908e1f9248de0dd3d5059d9c85e93052
Svenkia <contrinostefano4@gmail.com>
2026-02-07 10:05:33 +0100

    Quando il bottone viene cliccato, rimane nello stesso colore dell'hover finché non ne viene selezionato un altro

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,32 @@ import languages from "./docs/languages.js"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [active, setActive] = useState(null)
 
   function change(who) {
     console.log("Cliccato", who);
     setCount(who);
   }
 
-
   return <>
 
     <h1 className="title">Learn Web development</h1>
 
     <div className="btn-container">
-      {languages.map((Language) => {
-        return <button onClick={() => { change(Language.id - 1) }} id="btn" key={Language.id}>{Language.title}</button>
+      {languages.map((Language, index) => {
+        return <button onClick={() => {
+          change(index)
+          setActive(index)
+        }}
+          className={active === index ? "btn-pressed" : "btn"}
+          key={Language.id}>{Language.title}</button>
       })}
     </div>
 
     <div className="card">
 
-
-
-
-
       <h3 className="card-text">{languages[count].title}</h3>
       <p className="card-description">{languages[count].description}</p>
-
-
-
 
     </div>
   </>
